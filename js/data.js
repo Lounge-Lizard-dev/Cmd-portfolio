6f93cb2263430000e907743a535a81de2f564ae8
Lounge Lizard dev/ <86478645+Lounge-Lizard-dev@users.noreply.github.com>
2021-11-14 16:42:54 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,21 @@
+var data = {
+  name : [
+    {name: 'Name', value: 'YOUR NAME HERE'}
+  ],
+  bio: [
+    {name: 'Name', value: 'YOUR NAME HERE'},
+    {name: 'Email', value: 'YOUR EMAIL HERE'}
+  ],
+  github: [
+    {name: 'GitHub', value: 'YOUR GITHUB USERNAME HERE'}
+  ],
+  projects: [
+    {name: 'ProjectName1', value: 'PROJECT LINK HERE'},
+    {name: 'ProjectName1', value: 'PROJECT LINK HERE'}
+  ],
+  resume: 'ResumeName.pdf',
+  socials: [
+    {name: 'LinkedIn', value: 'SITE URL'},
+    {name: 'Github', value: 'SITE URL'}
+  ]
+}
